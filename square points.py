584b6527180474546cdf3931106e4098c3002389
Dhineshkumarraveendiran <46440489+Dhineshkumarraveendiran@users.noreply.github.com>
2019-02-12 19:49:12 +0530

    Create square points.py

---
+++ square points.py
@@ -0,0 +1,9 @@
+#q
+a,b=map(int,input().split())
+c,d=map(int,input().split())
+e,f=map(int,input().split())
+g,h=map(int,input().split())
+if(a+c+e+g==b+d+f+h):
+    print("yes")
+else:
+    print("no")
